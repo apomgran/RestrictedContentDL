c2ed14d4d0ed6bdf2944544572630c2900226381
apomgran <apomgran@gmail.com>
2025-04-22 01:50:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,17 +12,17 @@ YTUB_COOKIES = """
 #Write Cookies Here
 """
 
-API_ID = int(getenv("API_ID", "2820"))
-API_HASH = getenv("API_HASH", "7fc5b3569245497ab5eca3")
-BOT_TOKEN = getenv("BOT_TOKEN", "8019809817:AAHNHK9inNi04DQx8hk0uE4")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7303810912").split()))
-MONGO_DB = getenv("MONGO_DB", "mongodb+srv://ytpremium4434360:zxx1VPDzGW96Nrites=true&w=majority&appName=ItsSmartToolBot")
-LOG_GROUP = getenv("LOG_GROUP", "-10017356")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002183077"))
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
+MONGO_DB = getenv("MONGO_DB", "")
+LOG_GROUP = getenv("LOG_GROUP", "-")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-"))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "500"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
-AD_API = getenv("AD_API", "52b4a2cf4687d81e7d3f8f8e78cb")
+AD_API = getenv("AD_API", "")
 STRING = getenv("STRING", None)
 YT_COOKIES = getenv("YT_COOKIES", YTUB_COOKIES)
 DEFAULT_SESSION = getenv("DEFAUL_SESSION", None)  # added old method of invite link joining
